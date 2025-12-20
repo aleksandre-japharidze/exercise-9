ed5dd06dd4ae70f108c09d727e853e2a65690198
AleksandreJapharidze <alexanderjapharidze@gmail.com>
2025-12-20 16:42:12 +0400

    Python implemented

--- python/program.py
+++ python/program.py
@@ -2,44 +2,40 @@ import asyncio
 import aiohttp
 import time
 
-async def fetch_api(session, url, semaphore, max_timeout=0.8):
+urls = [
+    "https://ipinfo.io/161.185.160.93/geo",
+    "https://official-joke-api.appspot.com/random_joke",
+    "https://randomuser.me/api/",
+    "https://api.zippopotam.us/us/33162",
+    "https://api.agify.io?name=meelad",
+    "https://api.zippopotam.us/us/33162",
+    "https://api.nationalize.io?name=nathaniel",
+    "https://api.genderize.io?name=luc"
+]
+
+semaphore = asyncio.Semaphore(4)
+
+async def fetch_api(url, session, timeout=800):
     async with semaphore:
         print("FETCHING: " + url)
         start_time = time.time()
         try:
-            timeout = aiohttp.ClientTimeout(total=max_timeout)
-            async with session.get(url, timeout=timeout) as response:
-                elapsed = time.time() - start_time
+            fetch_timeout = aiohttp.ClientTimeout(total=timeout)
+            async with session.get(url, timeout=fetch_timeout) as response:
                 if response.status != 200:
-                    print("FAILED: " + url + " -- TIME: " + str(elapsed))
+                    print("FAILED: " + url + " -- TIME: " + str(time.time() - start_time))
                     return
-                else:
-                    print("SUCCESS: " + url + " -- TIME: " + str(elapsed))
+                print("SUCCESS: " + url + " -- TIME: " + str(time.time() - start_time))
         except asyncio.TimeoutError:
-            print("TIMEOUT: " + url + " -- TIME: " + str(elapsed))
+            print("TIMEOUT: " + url + " -- TIME: " + str(time.time() - start_time))
         except Exception as e:
             print("ERROR: " + e)
         finally:
             print("DONE: " + url)
 
 async def main():
-    urls = [
-        "https://ipinfo.io/161.185.160.93/geo",
-        "https://official-joke-api.appspot.com/random_joke",
-        "https://randomuser.me/api/",
-        "https://api.zippopotam.us/us/33162",
-        "https://api.agify.io?name=meelad",
-        "https://api.zippopotam.us/us/33162",
-        "https://api.nationalize.io?name=nathaniel",
-        "https://api.genderize.io?name=luc"
-    ]
+    session = aiohttp.ClientSession()
+    await asyncio.gather(*[fetch_api(url, session) for url in urls])
+    await session.close()
 
-    async with aiohttp.ClientSession() as session:
-        semaphore = asyncio.Semaphore(4)
-        tasks = [fetch_api(session, url, semaphore) for url in urls]
-        await asyncio.gather(*tasks)
-        
-        print("ALL DONE")
-
-if __name__ == "__main__":
-    asyncio.run(main())
+asyncio.run(main())
